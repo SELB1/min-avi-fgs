5c880dce43683929339db907efccfa74ff594b78
SELB1 <94957228+SELB1@users.noreply.github.com>
2023-12-03 23:58:43 +0100

    Update load_factors.py

--- fgs/functions/load_factors.py
+++ fgs/functions/load_factors.py
@@ -1,3 +1,31 @@
 """
 Donne nx, nz et nx_AP, nz_AP
 """
+from ivy.std_api import *
+
+def nx_control(nx):
+    print(f"NxControl received: nx = {nx}")
+
+def nz_control(nz):
+    print(f"NzControl received: nz = {nz}")
+
+def setup_ivy():
+    nh = lambda _x, _y: 1
+
+    IvyInit("Avionique",
+        "[%s ready]",
+        0,
+        nh,
+        nh
+    )
+
+    IvyBindMsg(nx_control, '^APNxControl nx=(\S+)')
+    IvyBindMsg(nz_control, '^APNzControl nz=(\S+)')
+    
+    IvyStart("10.1.127.255:2012")
+
+def send_nx_control(nx):
+    IvySendMsg(f"NxControl nx={nx}")
+
+def send_nz_control(nz):
+    IvySendMsg(f"NzControl nz={nz}")
