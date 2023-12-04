74fb2e8670e52b2f0e480daaa020eb8f6179d53a
SELB1 <94957228+SELB1@users.noreply.github.com>
2023-12-04 17:30:41 +0100

    Update load_factors.py

--- fgs/functions/load_factors.py
+++ fgs/functions/load_factors.py
@@ -3,11 +3,27 @@ Donne nx, nz et nx_AP, nz_AP
 """
 from ivy.std_api import *
 
+class StateVector:
+    def __init__(self, x:float, y:float, z:float, Vp:float, fpa:float, psi:float, phi:float):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.Vp = Vp
+        self.fpa = fpa
+        self.psi = psi
+        self.phi = phi
+
 def nx_control(nx):
     print(f"NxControl received: nx = {nx}")
 
+def nx_AP_control(nxAP):
+    print(f"NxAPControl received: nxAP = {nxAP}")
+
 def nz_control(nz):
     print(f"NzControl received: nz = {nz}")
+
+def nz_AP_control(nzAP):
+    print(f"NzAPControl received: nzAP = {nzAP}")
 
 def setup_ivy():
     nh = lambda _x, _y: 1
@@ -21,6 +37,8 @@ def setup_ivy():
 
     IvyBindMsg(nx_control, '^APNxControl nx=(\S+)')
     IvyBindMsg(nz_control, '^APNzControl nz=(\S+)')
+    IvyBindMsg(nx_AP_control, '^APNzControl nx=(\S+)')
+    IvyBindMsg(nz_AP_control, '^APNzControl nz=(\S+)')
     
     IvyStart("10.1.127.255:2012")
 
@@ -29,3 +47,9 @@ def send_nx_control(nx):
 
 def send_nz_control(nz):
     IvySendMsg(f"NzControl nz={nz}")
+
+def send_nx_AP_control(nz):
+    IvySendMsg(f"NxAPControl nxAP={nxAP}")
+
+def send_nz_AP_control(nz):
+    IvySendMsg(f"NzAPControl nzAP={nzAP}")
