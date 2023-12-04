d6db7c0ed9b262ec840ceed0c2931799f143b7a8
AlexBraziil <alex.brazil.0216@gmail.com>
2023-12-04 17:09:08 +0100

    grg

--- fgs/tests/exemple_test.py
+++ fgs/tests/exemple_test.py
@@ -2,7 +2,7 @@
 Modèle de rédaction de tests
 """
 
-from fgs.functions.example_function import sample_func
+from fgs.functions.altitude import altitude
 
 def example_success():
     return 0
@@ -11,4 +11,4 @@ def example_error():
     return 1
 
 def example():
-    return sample_func()
+    return altitude()
