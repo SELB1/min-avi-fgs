ddf33a7cfff95bd3d8c95a68aeb3e531c2c1ba5d
AlexBraziil <alex.brazil.0216@gmail.com>
2023-12-04 16:32:31 +0100

    angle

--- fgs/functions/angles.py
+++ fgs/functions/angles.py
@@ -1,5 +1,6 @@
 import time
 from ivy.std_api import *
+maneuvering_speed = 128
 class StateVector:
     def __init__(self, x:float, y:float, z:float, Vp:float, fpa:float, psi:float, phi:float):
         self.x = x
@@ -18,19 +19,18 @@ def callback(agent, *data):
 
 def send_bank_angle(state_vector):
     # Règles spécifiées pour l'angle de braquage en fonction de la vitesse de manoeuvre
-    maneuvering_speed = state_vector.Vp
-    if maneuvering_speed <= maneuvering_speed -10:
+    vp = state_vector.Vp
+    pmax, pmax_AP = 15
+    if vp <= maneuvering_speed -10:
+        bank_angle_max_AP= 15
         bank_angle_max = 15
-    elif maneuvering_speed <= (maneuvering_speed - 3):
-        bank_angle_max = 20
-    else:
+    elif vp > maneuvering_speed -10:
+        bank_angle_max = 25
+        bank_angle_max_AP=30
         # Choix entre la guidance latérale sélectionnée et gérée
-        if selected_lateral_guidance:
-            bank_angle_max = 25
-        else:
-            bank_angle_max = 30
-    IvySendMsg(f"^phimax={bank_angle_max}")
-    
+    IvySendMsg(f"RollLimits phimax={bank_angle_max} pmax={pmax}")
+    IvySendMsg(f"RollLimitsAP phimax_AP={bank_angle_max_AP} pmax_AP={pmax_AP}")
+
 # Définir la variable pour la guidance latérale sélectionnée (à remplacer par vos données réelles)
 selected_lateral_guidance = True
 
