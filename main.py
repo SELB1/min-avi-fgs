ff54edadb7ec7f5452c235fec7c4bb1e31332208
storca <storca@mail.com>
2023-12-04 14:46:08 +0100

    add StateVector object

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 """
 Fichier principal
 """
+class StateVector:
+    def __init__(self, x:float, y:float, z:float, Vp:float, fpa:float, psi:float, phi:float):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.Vp = Vp
+        self.fpa = fpa
+        self.psi = psi
+        self.phi = phi
+
