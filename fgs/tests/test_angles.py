2d4c59cffa26a1193635b364b1dc7c27e3182a76
SELB1 <94957228+SELB1@users.noreply.github.com>
2023-12-09 22:54:34 +0100

    Update test_angles.py

--- fgs/tests/test_angles.py
+++ fgs/tests/test_angles.py
@@ -1,15 +1,31 @@
 from ivy.std_api import IvyInit, IvyStart, IvyBindMsg, IvyMainLoop, IvyStop
+from fgs.defs import StateVector
 
 def null_cb(*a):
     pass
 
-def on_dirto(agent, *data):
-    print("DIRTO received: {}".format(data[0]))
+def on_roll_limits(agent, *data):
+    print("Roll limits received: {}".format(data))
+
+def test_send_bank_angle():
+    # Simuler un StateVector pour tester la fonction send_bank_angle
+    test_state_vector = StateVector(x=0.0, y=0.0, z=0.0, Vp=150.0, fpa=0.0, psi=0.0, phi=0.0)
+
+    # Initialisation Ivy avec des fonctions de rappel vides
+    IvyInit("TestSendBankAngle", "[%s ready]", 0, null_cb, null_cb)
+    IvyStart("127.255.255.255:2010")
+
+    # Liaison de la fonction de rappel à un motif de message Ivy
+    IvyBindMsg(on_roll_limits, "^RollLimits phimax=(\S+) pmax=(\S+)")
+
+    # Appel de la fonction à tester
+    send_bank_angle(test_state_vector)
+
+    # Boucle principale Ivy pour attendre les messages
+    IvyMainLoop()
+
+    # Arrêt d'Ivy après la boucle principale
+    IvyStop()
 
 if __name__ == "__main__":
-    IvyInit("TestDirto", "[%s ready]", 0, null_cb, null_cb)
-    IvyStart("10.1.127.255:2012")
-
-    IvyBindMsg(on_dirto, "^DIRTO x=(\S+) y=(\S+)")
-
-    IvyMainLoop()
+    test_send_bank_angle()
