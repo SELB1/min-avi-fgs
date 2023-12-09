ea5a3aa1bc65b6ab2e7f38e8851a9797ad7a7f3a
SELB1 <94957228+SELB1@users.noreply.github.com>
2023-12-09 22:57:53 +0100

    Update test_speeds.py

--- fgs/tests/test_speeds.py
+++ fgs/tests/test_speeds.py
@@ -1,15 +1,1 @@
-from ivy.std_api import IvyInit, IvyStart, IvyBindMsg, IvyMainLoop, IvyStop
 
-def null_cb(*a):
-    pass
-
-def on_speeds(agent, *data):
-    print("Speeds received: {}".format(data))
-
-if __name__ == "__main__":
-    IvyInit("TestSpeeds", "[%s ready]", 0, null_cb, null_cb)
-    IvyStart("10.1.127.255:2012")
-
-    IvyBindMsg(on_speeds, "^SpeedLimits vmin=(\S+) vmax=(\S+)")
-
-    IvyMainLoop()
