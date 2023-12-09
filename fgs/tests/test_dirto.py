47ec51e5c3657600cca04954598a966cba6db3d9
SELB1 <94957228+SELB1@users.noreply.github.com>
2023-12-09 22:56:57 +0100

    Update test_dirto.py

--- fgs/tests/test_dirto.py
+++ fgs/tests/test_dirto.py
@@ -6,10 +6,19 @@ def null_cb(*a):
 def on_dirto(agent, *data):
     print("DIRTO received: {}".format(data[0]))
 
-if __name__ == "__main__":
+def test_dirto():
+    # Initialisation Ivy avec des fonctions de rappel vides
     IvyInit("TestDirto", "[%s ready]", 0, null_cb, null_cb)
     IvyStart("10.1.127.255:2012")
 
+    # Liaison de la fonction de rappel à un motif de message Ivy correspondant à la fonction on_dirto
     IvyBindMsg(on_dirto, "^DIRTO x=(\S+) y=(\S+)")
 
+    # Boucle principale Ivy pour attendre les messages
     IvyMainLoop()
+
+    # Arrêt d'Ivy après la boucle principale
+    IvyStop()
+
+if __name__ == "__main__":
+    test_dirto()
