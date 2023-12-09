45de26b9212a7f95ee22fc97f6747b62f3e9f2db
SELB1 <94957228+SELB1@users.noreply.github.com>
2023-12-09 22:56:06 +0100

    Update test_load_factors.py

--- fgs/tests/test_load_factors.py
+++ fgs/tests/test_load_factors.py
@@ -6,10 +6,22 @@ def null_cb(*a):
 def on_loadfactors(agent, *data):
     print("Load factors received: {}".format(data))
 
-if __name__ == "__main__":
+def test_loadfactors():
+    # Initialisation Ivy avec des fonctions de rappel vides
     IvyInit("TestLoadFactors", "[%s ready]", 0, null_cb, null_cb)
     IvyStart("10.1.127.255:2012")
 
-    IvyBindMsg(on_loadfactors, "^NxControl nx_max=(\S+) nx_min=(\S+) nz_max=(\S+) nz_min=(\S+)")
+    # Liaison de la fonction de rappel à un motif de message Ivy correspondant à loadfactors.py
+    IvyBindMsg(on_loadfactors, "^NxControl nx_max=(\S+) nx_min=(\S+) nz_max=(\S+) nz_min=(\S+) NxAPControl nxAP=(\S+) NzAPControl nzAP=(\S+)")
 
+    # Appel de la fonction à tester
+    loadfactors()
+
+    # Boucle principale Ivy pour attendre les messages
     IvyMainLoop()
+
+    # Arrêt d'Ivy après la boucle principale
+    IvyStop()
+
+if __name__ == "__main__":
+    test_loadfactors()
