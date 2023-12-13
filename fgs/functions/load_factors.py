b2f404399b99b7d5dfb9598f6b59734292d107f0
storca <storca@mail.com>
2023-12-13 16:01:16 +0100

    Fix value unpack

--- fgs/functions/load_factors.py
+++ fgs/functions/load_factors.py
@@ -10,14 +10,12 @@ def callback(agent, *data):
 
 
 def loadfactors():
-    nx_pos, nx_pos_AP=2.5
-    nx_neg, nx_neg_AP=-1
-    nz_pos, nz_pos_AP=2.5
-    nz_neg, nz_neg_AP=-1
+    nx_pos, nx_pos_AP= (2.5, 2.5)
+    nx_neg, nx_neg_AP= (-1, -1)
+    nz_pos, nz_pos_AP= (2.5, 2.5)
+    nz_neg, nz_neg_AP= (-1, -1)
     IvySendMsg(f"LimitsNAP nx_neg_AP={nx_neg_AP} nx_pos_AP={nx_pos_AP} nz_neg_AP={nz_neg_AP} nz_pos_AP={nz_pos_AP}")
     IvySendMsg(f"LimitsN nx_neg={nx_neg} nx_pos={nx_pos} nz_neg={nz_neg} nz_pos={nz_pos}")
-    
-          
 
 def setup_ivy():
     nh = lambda _x, _y: 1
